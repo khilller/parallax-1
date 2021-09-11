b4b732e4d34121e3038b34a1e8eae14eb361a6b9
theArchitect <akhil.hem@outlook.com>
2021-09-10 22:41:07 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,192 @@
+// get reference to Canvas
+var canvas = document.getElementById('canvas');
+
+// get reference to canvas context
+var context = canvas.getContext('2d');
+
+//initialize loading variables
+var load_counter = 0;
+
+//initialize images for layers
+var grass = new Image();
+var paver = new Image();
+var outline = new Image();
+var trees = new Image();
+var humans = new Image();
+
+//create a lis of layer objects
+
+var layer_list = [
+    {
+        'image': grass,
+        'src': './Assets/05-grass.png',
+        'z_index': -2.0,
+        'position': {x:0 , y:0},
+        'blend': null,
+        'opacity': 1
+    },
+    
+    {
+        'image': paver,
+        'src': './Assets/04-Paver.png',
+        'z_index': -1.5,
+        'position': {x:0 , y:0},
+        'blend': null,
+        'opacity': 1
+    },
+
+
+    {
+        'image': outline,
+        'src': './Assets/03-Outline.png',
+        'z_index': -1.0,
+        'position': {x:0 , y:0},
+        'blend': null,
+        'opacity': 1
+    },
+
+    {
+        'image': trees,
+        'src': './Assets/02-Trees.png',
+        'z_index': -0.5,
+        'position': {x:0 , y:0},
+        'blend': null,
+        'opacity': 1
+    },
+
+    {
+        'image': humans,
+        'src': './Assets/01-Humans.png',
+        'z_index': 0.5,
+        'position': {x:0 , y:0},
+        'blend': null,
+        'opacity': 0.9
+    }
+];
+
+layer_list.forEach(function(layer,index){
+    layer.image.onload = function() {
+        load_counter += 1;
+        if (load_counter >= layer_list.length) {
+            requestAnimationFrame(drawCanvas);
+        }
+    }
+    layer.image.src = layer.src;
+    
+});
+
+function drawCanvas() {
+    //clear what is in the canvas
+    context.clearRect(0,0,canvas.width, canvas.height);
+
+    //loop thriugh each ;ayer and draw it to the canvas
+    layer_list.forEach(function(layer, index){
+
+        layer.position = getOffset(layer);
+
+
+
+        if (layer.blend) {
+            context.globalCompositeOperation = layer.blend;
+        } else {
+            context.globalCompositeOperation = 'normal';
+        }
+
+        context.globalAlpha = layer.opacity;
+
+        context.drawImage(layer.image, layer.position.x, layer.position.y);
+    });
+    requestAnimationFrame(drawCanvas);
+}
+
+function getOffset(layer) {
+    var touch_multiplier = 0.1;
+    var touch_offset_x = pointer.x * layer.z_index * touch_multiplier;
+    var touch_offset_y = pointer.y * layer.z_index * touch_multiplier;
+
+    var offset = {
+        x: touch_offset_x,
+        y: touch_offset_y
+    };
+
+    return offset;
+}
+
+
+
+
+/// touch and mouse controls ///
+
+var moving = false;
+
+// Initialize touch and mouse position
+var pointer_initial = {
+    x:0,
+    y:0
+}
+
+var pointer = {
+    x:0,
+    y:0,
+}
+
+canvas.addEventListener('touchstart', pointerStart);
+canvas.addEventListener('mousedown', pointerStart);
+
+
+function pointerStart(event) {
+    moving = true;
+    if (event.type === 'touchstart') {
+        pointer_initial.x = event.touches[0].clientX;
+        pointer_initial.y = event.touches[0].clientY;
+    } else if (event.type === 'mousedown') {
+        pointer_initial.x = event.clientX;
+        pointer_initial.y = event.clientY;
+    }
+}
+
+
+window.addEventListener('touchmove', pointerMove);
+window.addEventListener('mousemove', pointerMove)
+
+function pointerMove(event) {
+    event.preventDefault();
+    if (moving === true) {
+        var current_x = 0;
+        var current_y = 0;
+        if (event.type === 'touchmove') {
+            current_x = event.touches[0].clientX;
+            current_y = event.touches[0].clientY;
+        } else if (event.type === 'mousemove') {
+            current_x = event.clientX;
+            current_y = event.clientY;
+        }
+        pointer.x = current_x - pointer_initial.x;
+        pointer.y = current_y - pointer_initial.y;
+    }
+
+}
+
+canvas.addEventListener('touchmove', function(event){
+    event.preventDefault();
+});
+
+canvas.addEventListener('mousemove', function(event){
+    event.preventDefault();
+});
+
+window.addEventListener('touchend', function(event){
+    endGesture();
+})
+
+window.addEventListener('mouseup', function(event){
+    endGesture();
+})
+
+function endGesture() {
+    moving = false;
+
+    pointer.x = 0;
+    pointer.y = 0
+
+}
